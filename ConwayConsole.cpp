7b3563b4bc266ffd573c3cfba6f093298e9ba267
Leif-Erik Schmitz <leif-erik.schmitz@web.de>
2021-01-31 12:34:51 +0100

    Manual steps and 2 simple shapes

--- ConwayConsole.cpp
+++ ConwayConsole.cpp
@@ -15,10 +15,57 @@ std::vector<std::vector<cell_t>> field(num_rows, std::vector<cell_t>(num_cols));
 //DEFINE FUNCTIONS
 void print_field(unsigned int rows, unsigned int cols);
 
-void count_neighbors(cell_t &cell, unsigned int x, unsigned int y) {
+void count_neighbors(cell_t& cell, unsigned int x, unsigned int y);
+
+void update_neighbor_count();
+
+void update_field();
+
+void init_vector();
+
+void spawn_blinker(unsigned int x, unsigned int y);
+
+void spawn_block(unsigned int x, unsigned int y);
+
+void spawn_beacon(unsigned int x, unsigned int y);
+
+void run(char user_input, bool& running);
+
+int main()
+{
+    init_vector();
+    print_field(num_rows, num_cols);
+    bool running = true;
+    char user_input;
+    while (running) {
+        std::cin >> user_input;
+        run(user_input, running);
+        
+    }
+
+    return 0;
+
+}
+
+
+//FUNTION IMPLENTATION
+void print_field(unsigned int rows, unsigned int cols) {
+    for (unsigned int i = 0; i < rows; ++i) {
+        for (unsigned int j = 0; j < cols; ++j) {
+            if (field[i][j].alive)
+                std::cout << (char)254;
+            else
+                std::cout << field[i][j].alive_neighbors;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void count_neighbors(cell_t& cell, unsigned int x, unsigned int y) {
     unsigned int count = 0;
-    for (int i = x-1; i <= x+1; i++) {
-        for (int j = y-1; j <= y+1; j++) {
+    for (int i = x - 1; i <= x + 1; i++) {
+        for (int j = y - 1; j <= y + 1; j++) {
             if (i == x && j == y)
                 continue;
 
@@ -26,7 +73,7 @@ void count_neighbors(cell_t &cell, unsigned int x, unsigned int y) {
                 if (field[i][j].alive) {
                     count++;
                 }
-            }          
+            }
         }
     }
     cell.alive_neighbors = count;
@@ -39,9 +86,9 @@ void update_neighbor_count() {
         }
     }
 }
- 
+
 void update_field() {
-    
+
 
     for (int i = 0; i < num_rows; i++) {
         for (int j = 0; j < num_cols; j++) {
@@ -67,32 +114,42 @@ void init_vector() {
             field[i][j] = new_cell;
         }
     }
-    field[num_rows / 2][num_cols / 2].alive = true;
+    spawn_beacon((num_rows / 2), (num_cols / 2));
     update_neighbor_count();
 }
 
-int main()
-{
-    init_vector();
+//SPAWN SHAPES
 
+void spawn_blinker(unsigned int x, unsigned int y) {
+    field[x - 1][y].alive = true;
+    field[x][y].alive = true;
+    field[x + 1][y].alive = true;
+}
 
-    print_field(num_rows, num_cols);
+void spawn_block(unsigned int x, unsigned int y) {
+    field[x][y].alive = true;
+    field[x][y + 1].alive = true;
+    field[x + 1][y].alive = true;
+    field[x + 1][y + 1].alive = true;
+}
 
-    return 0;
-
+void spawn_beacon(unsigned int x, unsigned int y) {
+    spawn_block(x, y);
+    spawn_block(x + 2, y + 2);
 }
 
 
-//FUNTION IMPLENTATION
-void print_field(unsigned int rows, unsigned int cols) {
-    for (unsigned int i = 0; i < rows; ++i) {
-        for (unsigned int j = 0; j < cols; ++j) {
-            if (field[i][j].alive)
-                std::cout << (char)254;
-            else
-                std::cout << field[i][j].alive_neighbors;
-        }
-        std::cout << std::endl;
+//USER INTERACTION
+void run(char user_input, bool& running) {
+    switch (user_input)
+    {
+    case 'n':
+        update_field();
+        break;
+    default:
+        running = false;
+        break;
     }
-    std::cout << std::endl;
+    system("CLS");
+    print_field(num_rows, num_cols);
 }
